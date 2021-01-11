b76680321ad20c144a4c6bf132f6b814a31d8e7b
kenorb <kenorb@users.noreply.github.com>
2021-01-11 02:41:21 +0000

    Optimize SignalOpen params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -7,7 +7,7 @@
 struct Indi_Envelopes_Params_M5 : Indi_Envelopes_Params {
   Indi_Envelopes_Params_M5() : Indi_Envelopes_Params(indi_env_defaults, PERIOD_M5) {
     applied_price = (ENUM_APPLIED_PRICE)5;
-    deviation = 0.1;
+    deviation = 0;
     ma_method = 1;
     ma_period = 24;
     ma_shift = 0;
@@ -20,14 +20,14 @@ struct Stg_Envelopes_Params_M5 : StgParams {
   // Struct constructor.
   Stg_Envelopes_Params_M5() : StgParams(stg_env_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 1;
+    signal_open_method = -4;
+    signal_open_filter = 0;
     signal_open_level = (float)0.0;
-    signal_open_boost = 0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)0;
+    signal_close_level = (float)0.0;
     price_stop_method = 0;
-    price_stop_level = (float)1;
+    price_stop_level = (float)0.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
