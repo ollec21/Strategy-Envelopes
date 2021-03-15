e2367f75e16ef81d85d12cb4e6d4fbc7bce19050
ollec21 <ollec21@users.noreply.github.com>
2021-03-15 16:34:12 +0000

    Optimize Indicator params (pf=1.02; dd=0.81; tt=757)

--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -7,7 +7,7 @@
 struct Indi_Envelopes_Params_M30 : EnvelopesParams {
   Indi_Envelopes_Params_M30() : EnvelopesParams(indi_env_defaults, PERIOD_M30) {
     applied_price = (ENUM_APPLIED_PRICE)2;
-    deviation = 0.1;
+    deviation = 0.07;
     ma_method = (ENUM_MA_METHOD)2;
     ma_period = 15;
     ma_shift = 3;
